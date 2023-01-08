a2ccebe92feab2ecd70dcbc239d6a625685d08df
andrey321111 <andreyaafo@gmail.com>
2023-01-08 20:53:50 +0200

    Escape Sequences in Strings

---
+++ JavaScript/ls27.js
@@ -0,0 +1,19 @@
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+
+
+/*
+
+Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\t	tab
+\r	carriage return
+\b	word boundary
+\f	form feed
+
+
+*/
