ee448c68ef59622a8879d20cde43e9fbed94b09d
andrey321111 <andreyaafo@gmail.com>
2023-01-09 20:22:16 +0200

    Word Blanks

---
+++ JavaScript/ls39.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "the "+myNoun+" is "+ myAdjective+" and it "+myVerb+" very "+myAdverb; // Change this line
+// Only change code above this line
